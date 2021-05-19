2174b7013d8d576f44c7a944c44e896ade88d429
djb91c <djb91c@gmail.com>
2021-05-19 08:52:30 -0400

    transitioning to sequelize

---
+++ db/startData.js
@@ -0,0 +1,11 @@
+startData = [
+{name: 'React', description:'used to create user interfaces', weekly_downloads: '10,965,933', num_dependencies: 2},
+{name: 'prop-types', description:'documents intended types of React properties passed to components', weekly_downloads:'11,221,263', num_dependencies: 3},
+{name: 'Movement', description:'time manipulation library', weekly_downloads:'17,736,573', num_dependencies: 0},
+{name: 'Express', description:'webframe for node', weekly_downloads:'17,128,152', num_dependencies: 30},
+{name: 'Lodash', description:'utility library with methods for dealing with arrays, numbers, objects, etc.', weekly_downloads:'44,617,034', num_dependencies: 0},
+{name: 'Async', description:'provides functions for dealing with asynchronous Javascript', weekly_downloads:'41,105,929', num_dependencies:0},
+{name:'Chalk', description:'used to style terminal strings', weekly_downloads:'94,153,123', num_dependencies:2},
+{name:'Commander', description:'lightweight solution for creating CLI apps', weekly_downloads:'62,932,876', num_dependencies:0},
+{name:'Debug', description:'debugging utility modeled after the node.js debugging model', weekly_downloads:'109,265,683', num_dependencies:1}
+]
